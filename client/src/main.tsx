87b26fece9ca6c919b8a4bcbc82fcdfee61a0c72
Artymiik <aworkprogramm@gmail.com>
2024-08-24 20:36:48 +0500

    add analytics vercal

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={App} />
+    <Analytics />
   </StrictMode>
 );
